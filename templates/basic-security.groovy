80e9086ac989e4895a8e23589ab4e9c1f9c2fbcd
Polyglot Programmer <muddasani.polyglot@gmail.com>
2016-12-30 17:28:20 +0530

    fixed the admin/admin user creation issue
    The community cookbook has an issue.

--- templates/basic-security.groovy
+++ templates/basic-security.groovy
@@ -3,13 +3,14 @@ import hudson.security.*
 import jenkins.model.*
 
 def instance = Jenkins.getInstance()
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+def users = hudsonRealm.getAllUsers()
+users_s = users.collect { it.toString() }
+if ("{{ jenkins_admin_username }}" in users_s) {
+    println "Admin user already exists"
+} else {
+    println "--> creating local admin user"
 
-println "--> Checking if security has been set already"
-
-if (!instance.isUseSecurity()) {
-    println "--> creating local user 'admin'"
-
-    def hudsonRealm = new HudsonPrivateSecurityRealm(false)
     hudsonRealm.createAccount('{{ jenkins_admin_username }}', '{{ jenkins_admin_password }}')
     instance.setSecurityRealm(hudsonRealm)
 
